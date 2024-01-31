e735b958dde995eeccd86abf7270d1289c07b709
robbiedantonio <robbie.dantonio@gmail.com>
2024-01-31 18:38:59 -0500

    Fixed initialization

--- lawnmower_new.py
+++ lawnmower_new.py
@@ -30,6 +30,10 @@ log_file = open('HoceanLog.txt', 'a')   # Set to only append
 
 #### RUN SIMULATION
 num_timesteps = 10000    # Number of ticks in environment
+
+t = np.arange(0, num_pings)
+r = np.linspace(-maxR, maxR, binsR)
+R, T = np.meshgrid(r, t)
 data = np.zeros_like(R)
 
 with holoocean.make(scenario) as env:
