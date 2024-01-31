31de6e8dd996e5b661b5e25035932f3d0eb5e2a1
robbiedantonio <robbie.dantonio@gmail.com>
2024-01-31 18:34:15 -0500

    Added logging functionality for sonar data:

--- lawnmower_new.py
+++ lawnmower_new.py
@@ -4,7 +4,7 @@ import holoocean
 import matplotlib.pyplot as plt
 import numpy as np
 
-## Basic Configuration
+## Basic HoloOcean Configuration Stuff
 scenario = "OpenWater-TorpedoSidescanSonar"
 config = holoocean.packagemanager.get_scenario(scenario)
 config = config['agents'][0]['sensors'][-1]["configuration"]
@@ -20,8 +20,13 @@ Takes in a 5 length vector. The first element is the right fin
 angle from -45 to 45 degrees, then top, left, and bottom. The 
 last element is the “thruster” with a value of -100 to 100.
 '''
+## Initialize UUV controls setup - 20% thrust and all fins in neutral positions
 command = np.array([0,0,0,0,20])
 
+
+
+## Open log file to record simulation data 
+log_file = open('HoceanLog.txt', 'a')   # Set to only append
 
 #### RUN SIMULATION
 num_timesteps = 10000    # Number of ticks in environment
@@ -31,25 +36,24 @@ with holoocean.make(scenario) as env:
         env.act("auv0", command)
         state = env.tick()
 
-        if(num_timesteps % 500  == 0):
-        	# Attempt to turn the UUV
-        	command = [0,45,0,0,20]
+        if 'SidescanSonar' in state:
+            data = np.roll(data, 1, axis=0)
+            data[0] = state['SidescanSonar']
 
-        # if 'SidescanSonar' in state:
-        #     data = np.roll(data, 1, axis=0)
-        #     data[0] = state['SidescanSonar']
 
-        #     print(data)
+        log_file.write(data[0])
+        '''
+        CONTROL PLAN GOES HERE
+        '''
+        if(num_timesteps == 1000):          # Turn top rudder 45º 
+            # Attempt to turn the UUV
+            command = [0,45,0,0,20]
 
-        #     plt.set_array(data.ravel())
 
-        #     plt.draw()
-        #     plt.gcf().canvas.flush_events()
+        if(num_timesteps == 2000)
 
+        
 print("Finished Simulation!")
-plt.ioff()
-plt.show()
-
 
 
 
