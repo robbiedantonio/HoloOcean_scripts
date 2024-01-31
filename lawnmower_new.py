388dea693afcac8bdced94a181b62dfd5c2b965d
robbiedantonio <robbie.dantonio@gmail.com>
2024-01-31 18:41:45 -0500

    Fixed bug with writing to log file

--- lawnmower_new.py
+++ lawnmower_new.py
@@ -44,7 +44,8 @@ with holoocean.make(scenario) as env:
         if 'SidescanSonar' in state:
             data = np.roll(data, 1, axis=0)
             data[0] = state['SidescanSonar']
-            log_file.write(data[0])
+            sonar_str = np.array2string(data[0])
+            log_file.write(sonar_str)
         '''
         CONTROL PLAN GOES HERE
         '''
